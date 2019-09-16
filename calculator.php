dc215d304a200fec44826d799d0624ba0d10959a
Ross Hill <ross.hill@nucleusfinancial.com>
2019-09-16 14:55:43 +0100

    Calculator

--- calculator.php
+++ calculator.php
@@ -7,5 +7,14 @@
     </head>
     <body>
     <h1>Calculator<h1>
+    <form action="calculator.php" method="get">
+        <input type="number" name="num1">
+        <br>
+        <input type="number" name="num2">
+        <input type="submit">
+
+    </form>
+
+    <?php echo $_GET["num1"] + $_GET["num2"]?>
     </body>
 </html>
