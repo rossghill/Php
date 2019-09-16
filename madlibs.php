5d501b971e226d3910b876cf76d3f82f34e7327d
Ross Hill <ross.hill@nucleusfinancial.com>
2019-09-16 15:16:51 +0100

    Madlibs simulator

---
+++ madlibs.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Madlibs</title>
+</head>
+<body>
+
+    <form action="madlibs.php" method="get">
+        Color: <input type="text" name="color">
+        <br>
+        Plural Noun: <input type="text" name="pluralNoun">
+        <br>
+        Celebrity: <input type="text" name="celebrity">
+        <input type="submit">
+    </form>
+
+    <?php 
+
+        $color = $_GET["color"];
+        $pluralNoun = $_GET["pluralNoun"];
+        $celebrity = $_GET["celebrity"];
+
+        echo "Roses are $color <br>";
+        echo "$pluralNoun are blue <br>";
+        echo "I love $celebrity";
+    
+    
+    ?> 
+</body>
+</html>
